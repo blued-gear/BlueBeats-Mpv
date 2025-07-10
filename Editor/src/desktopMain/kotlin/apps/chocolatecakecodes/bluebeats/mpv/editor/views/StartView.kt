0d0a9f9c1738f69f768ddc700ee0c4739869f403
blued_gear <chocolatecakecodes@disroot.org>
2025-07-10 17:08:24 +0200

    Editor: show loading-indicator in StartView

--- Editor/src/desktopMain/kotlin/apps/chocolatecakecodes/bluebeats/mpv/editor/views/StartView.kt
+++ Editor/src/desktopMain/kotlin/apps/chocolatecakecodes/bluebeats/mpv/editor/views/StartView.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import apps.chocolatecakecodes.bluebeats.mpv.editor.LoadedFile
+import apps.chocolatecakecodes.bluebeats.mpv.editor.utils.Logger
 import com.dokar.sonner.*
 import io.github.vinceglb.filekit.absoluteFile
 import io.github.vinceglb.filekit.absolutePath
@@ -18,42 +22,67 @@ import io.github.vinceglb.filekit.dialogs.compose.SaverResultLauncher
 import io.github.vinceglb.filekit.dialogs.compose.rememberFilePickerLauncher
 import io.github.vinceglb.filekit.dialogs.compose.rememberFileSaverLauncher
 import io.github.vinceglb.filekit.path
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 internal fun StartView(onContinue: () -> Unit) {
     val toaster = rememberToasterState()
-    val onClickNewHandler = onClickNew(onContinue)
-    val onClickOpenHandler = onClickOpen(toaster, onContinue)
+    val loading = remember { mutableStateOf(false) }
+    val onClickNewHandler = onClickNew(loading, onContinue)
+    val onClickOpenHandler = onClickOpen(loading, toaster, onContinue)
 
     Toaster(toaster, alignment = Alignment.TopEnd)
 
-    Row(
-        modifier = Modifier.safeContentPadding().fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = Modifier.fillMaxSize().safeContentPadding(),
+        verticalArrangement = Arrangement.Center,
     ) {
-        Button(onClick = { onClickNewHandler.launch("playlist", "bbdp") }) {
-            Text("New File")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Button(
+                enabled = !loading.value,
+                onClick = { onClickNewHandler.launch("playlist", "bbdp") }
+            ) {
+                Text("New File")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                enabled = !loading.value,
+                onClick = { onClickOpenHandler.launch() }
+            ) {
+                Text("Open File")
+            }
         }
-        Spacer(modifier = Modifier.width(16.dp))
-        Button(onClick = { onClickOpenHandler.launch() }) {
-            Text("Open File")
+
+        if(loading.value) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Text("Loading...")
+            }
         }
     }
 }
 
 @Composable
-private fun onClickNew(onContinue: () -> Unit): SaverResultLauncher {
+private fun onClickNew(loading: MutableState<Boolean>, onContinue: () -> Unit): SaverResultLauncher {
     return rememberFileSaverLauncher { file ->
         if(file == null) return@rememberFileSaverLauncher
 
+        loading.value = true
         LoadedFile.initEmpty(file.absolutePath())
         onContinue()
     }
 }
 
 @Composable
-private fun onClickOpen(toaster: ToasterState, onContinue: () -> Unit): PickerResultLauncher {
+private fun onClickOpen(loading: MutableState<Boolean>, toaster: ToasterState, onContinue: () -> Unit): PickerResultLauncher {
     return rememberFilePickerLauncher(
         title = "Open DynPl File",
         mode = FileKitMode.Single,
@@ -61,13 +90,24 @@ private fun onClickOpen(toaster: ToasterState, onContinue: () -> Unit): PickerRe
     ) { file ->
         if(file == null) return@rememberFilePickerLauncher
 
-        try {
-            LoadedFile.load(file.absoluteFile().path)
-            onContinue()
-        } catch(e: Exception) {
-            e.printStackTrace()
-            toaster.show("Unable to load file\n${e.localizedMessage}",
-                type = ToastType.Error, duration = ToasterDefaults.DurationLong)
+        loading.value = true
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                LoadedFile.load(file.absoluteFile().path)
+
+                withContext(Dispatchers.Main) {
+                    onContinue()
+                }
+            } catch(e: Exception) {
+                Logger.error("StartView", "exception in LoadedFile::load", e)
+
+                withContext(Dispatchers.Main) {
+                    toaster.show(
+                        "Unable to load file\n${e.localizedMessage}",
+                        type = ToastType.Error, duration = ToasterDefaults.DurationLong
+                    )
+                }
+            }
         }
     }
 }
